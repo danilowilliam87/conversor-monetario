728d54107c6e43cd5f40c21eab45bbee085ef1d9
danilo <danilowilliam87@outlook.com>
2023-07-09 13:58:39 -0300

    test : teste referente a o conversor da moeda Libra ESterlina OK [ OBS : a api nao dispoe da cotacao Libra esterlina para Peso Argentino e Peso chileno portanto o teste nao foi feito]

--- src/main/java/com/io/main/Main.java
+++ src/main/java/com/io/main/Main.java
@@ -14,30 +14,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    	BigDecimal valor = new BigDecimal(1.00);
-    	Euro euro = new Euro(valor);
+    	BigDecimal valor = new BigDecimal(10.00);
+    	Euro euro = new Euro();
     	Real real = new Real();
     	Dolar dolar = new Dolar();
-    	LibraEsterlina lb = new LibraEsterlina();
+    	LibraEsterlina lb = new LibraEsterlina(valor);
     	PesoArgentino pa = new PesoArgentino();
     	PesoChileno pc = new PesoChileno();
     	ConversorImpl conversorImpl = new ConversorImpl();
     	
-    	
+    	System.out.println("Libras Esterlinas em Reais : " + conversorImpl.converter(lb, real));
+    	System.out.println("Libras Esterlinas em Dolar : " + conversorImpl.converter(lb, dolar));
+    	System.out.println("Libras Esterlinas em Euro : " + conversorImpl.converter(lb, euro));
+//    	System.out.println("Libras Esterlinas em Peso Argentino : " + conversorImpl.converter(lb, pa));
+    	System.out.println("Libras Esterlinas em Peso Chileno : " + conversorImpl.converter(lb, pc));
 //    	System.out.println("euro em reais : " + conversorImpl.converter(euro, real));
 //    	System.out.println("euro em dolar : " + conversorImpl.converter(euro, dolar));
 //    	System.out.println("euro em libras : " + conversorImpl.converter(euro, lb));
 //    	System.out.println("euro em peso argentino : " + conversorImpl.converter(euro, pa));
 //    	System.out.println("euro em peso chileno : " + conversorImpl.converter(euro, pc));
     	
-    	String moeda1 = euro.getClass().toString();
-    	String moeda2 = euro.getClass().toString();
-    	
-    	if(moeda1.equals(moeda2)) {
-    		System.out.println("as moedas são iguais");
-    	}else {
-    		System.out.println("as moedas são diferentes");
-    	}
+//    	String moeda1 = euro.getClass().toString();
+//    	String moeda2 = euro.getClass().toString();
+//    	
+//    	if(moeda1.equals(moeda2)) {
+//    		System.out.println("as moedas são iguais");
+//    	}else {
+//    		System.out.println("as moedas são diferentes");
+//    	}
 
 	}
 
--- src/test/java/com/io/conversortest/ConversorLibraEsterlinaTest.java
+++ src/test/java/com/io/conversortest/ConversorLibraEsterlinaTest.java
@@ -7,8 +7,15 @@ import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
 
+import com.io.dominio.Dolar;
+import com.io.dominio.Euro;
 import com.io.dominio.LibraEsterlina;
+import com.io.dominio.Moeda;
+import com.io.dominio.PesoArgentino;
+import com.io.dominio.PesoChileno;
+import com.io.dominio.Real;
 import com.io.excecao.ValorInvalidoException;
+import com.io.impl.ConversorImpl;
 
 class ConversorLibraEsterlinaTest {
 
@@ -44,5 +51,37 @@ class ConversorLibraEsterlinaTest {
 		LibraEsterlina le = new LibraEsterlina(valor);
 		assertEquals("£100.00", le.toString());
 	}
+	
+	@Test
+	void converterLibraEsterlinaParaReal() {
+		BigDecimal valor = new BigDecimal(10.0);
+		LibraEsterlina le = new LibraEsterlina(valor);
+		Moeda moeda = new Real();
+		ConversorImpl impl = new ConversorImpl();
+		String retorno = impl.converter(le, moeda).toString();
+		assertEquals("R$ 62,55", retorno);
+	}
+	
+	@Test
+	void converterLibraEsterlinaParaDolar() {
+		BigDecimal valor = new BigDecimal(10.0);
+		LibraEsterlina le = new LibraEsterlina(valor);
+		Moeda moeda = new Dolar();
+		ConversorImpl impl = new ConversorImpl();
+		String retorno = impl.converter(le, moeda).toString();
+		assertEquals("$12.84", retorno);
+	}
+	
+	@Test
+	void converterLibraEsterlinaParaEuro() {
+		BigDecimal valor = new BigDecimal(10.0);
+		LibraEsterlina le = new LibraEsterlina(valor);
+		Moeda moeda = new Euro();
+		ConversorImpl impl = new ConversorImpl();
+		String retorno = impl.converter(le, moeda).toString();
+		assertEquals("11,70 €", retorno);
+	}
+	
+	
 
 }
